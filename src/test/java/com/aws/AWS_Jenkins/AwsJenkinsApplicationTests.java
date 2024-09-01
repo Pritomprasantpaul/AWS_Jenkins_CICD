cd092180e56d04217d9b25434e50f1bcf78ebaab
Pritom Paul <DIV@LAPTOP-C4AE14QO>
2024-09-01 20:41:20 +0530

    First Commit

---
+++ src/test/java/com/aws/AWS_Jenkins/AwsJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.AWS_Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
